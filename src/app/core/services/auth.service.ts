529092ef6004c935bce7d7891d6e6baceb17ad53
khaled <dev.khaled.mohamed@gmail.com>
2025-12-26 19:11:25 +0200

    1

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor() {
+ 
+   }
+   http = inject(HttpClient);
+
+   apiUrl = 'http://api.alquran.cloud/v1/edition';
+
+   fetchData(): Observable<any> {
+     return this.http.get(this.apiUrl);
+   }
+
+
+
 }
